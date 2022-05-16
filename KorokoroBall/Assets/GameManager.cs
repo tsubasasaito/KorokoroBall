e41ed825efd30c3557ceae0ba10136574ef0dd4a
SaitoTsubasa <k021g1226@g.neec.ac.jp>
2022-05-16 12:11:58 +0900

    4_01課題提出

---
+++ KorokoroBall/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeScene(string nextScene)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+    public void SceneReset()
+    {
+        string activeSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(activeSceneName);
+    }
+}
